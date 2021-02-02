9eb364470c2fd5b6198565f7ac4063da1a6a29a5
Hua Yuen <huayuenh@ie.ibm.com>
2021-02-02 15:04:00 +0000

    test

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -4,5 +4,5 @@ npm install
 npm run start-server & npm run test-fvt
 npm run stop-server
 #node test.js & npm run test-fvt
-FILE_LOCATIONS="./test/fvt-test.json"
+FILE_LOCATIONS="test/fvt-test.json"
 TEST_TYPES="fvt"
